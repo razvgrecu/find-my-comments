0a76da296505687667bc72899b0327e5460955f5
Razvan Grecu <razvan.grecu@fxbits.io>
2024-11-12 10:13:16 +0200

    Cleanup usage of the client.

--- src/main/java/dev/stephanson/find_my_comments/bitbucket/service/BitBucketService.java
+++ src/main/java/dev/stephanson/find_my_comments/bitbucket/service/BitBucketService.java
@@ -1,10 +1,8 @@
 package dev.stephanson.find_my_comments.bitbucket.service;
 
 import dev.stephanson.find_my_comments.bitbucket.client.BitbucketPullRequestClient;
-import dev.stephanson.find_my_comments.bitbucket.domain.PullRequestId;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,11 +15,5 @@ public class BitBucketService {
 
     @EventListener
     public void onStartup(ContextRefreshedEvent ignored) {
-        var x = client.listPullRequestComments(
-            PullRequestId.of(11598),
-            Pageable.ofSize(10)
-        ).blockFirst();
-
-        System.out.println(x);
     }
 }
